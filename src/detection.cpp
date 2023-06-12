cf506d2c97253f48d603db7843a4ce168be296e6
Jeff Johnson <jeff@fpgadeveloper.com>
2023-06-12 14:30:09 -0400

    * fix issue with cam2

--- src/detection.cpp
+++ src/detection.cpp
@@ -213,15 +213,15 @@ void Detection()
 		mipimediadev = findmipimedianode(mipi_type);
 		std::string tmp;
 		std::cout << "Configuring ";
-		if (strcmp("isp_vcap_csi", mipi_type.c_str()) == 0)
-		{
-			std::cout << "isp ";
-			tmp = "init-isp-smartvision.sh '" + mipimediadev + "'";
-		}
-		else
+		if (strcmp("vcap_rpi_cam1", mipi_type.c_str()) == 0)
 		{
 			std::cout << "RPi cam1 ";
 			tmp = "init-rpi-cam1-smartvision.sh '" + mipimediadev + "'";
+		}
+		else
+		{
+			std::cout << "RPi cam2 ";
+			tmp = "init-rpi-cam2-smartvision.sh '" + mipimediadev + "'";
 		}
 		std::cout << "node for RGB/1024x768 pipeline \n" ;
 				//  << tmp << std::endl;
